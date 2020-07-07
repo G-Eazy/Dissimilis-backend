0ba01ee1e9ee8970e348d5700dce5958e706ff8a
olivernilssen <oliver.nilssen@ciber.no>
2020-07-07 10:45:33 +0200

    added a few things to seeder before creating migration

--- Dissimilis.WebAPI/Database/Models/DissimilisSeeder.cs
+++ Dissimilis.WebAPI/Database/Models/DissimilisSeeder.cs
@@ -17,37 +17,49 @@ namespace Dissimilis.WebAPI.Database
 			//If null, add a usergroup
 			if (Instructor == null)
 			{
+				//Add some different groups, new users that will automatically be guest
 				context.UserGroups.Add(new UserGroup("Admin"));
+				context.UserGroups.Add(new UserGroup("Guest"));
+				context.UserGroups.Add(new UserGroup("Instructor"));
 			}
+
+			var Resources = context.Resources.FirstOrDefault(x => x.Name == "Read");
+			if (Resources is null)
+            {
+				//Add some different kinds of resources to access
+				context.Resources.Add(new Resource() { Name = "Read" });
+				context.Resources.Add(new Resource() { Name = "Write" });
+				context.Resources.Add(new Resource() { Name = "Delete" });
+				context.Resources.Add(new Resource() { Name = "Copy" });
+				context.Resources.Add(new Resource() { Name = "Update" });
+            }
 
 			var Organisation = context.Organisations.FirstOrDefault(x => x.Name == "Dissimilis Norge");
 			if (Organisation is null)
             {
 				context.Organisations.Add(new Organisation("Dissimilis Norge"));
-            }
+				context.Organisations.Add(new Organisation("Dissimilis Kultursenter"));
+			}
 
 			var Sweden = context.Countries.FirstOrDefault(x => x.Name == "Norway");
 			if (Sweden is null)
 			{
 				context.Countries.Add(new Country("Norway"));
-			}
-
-			var AccessRight = context.Resources.FirstOrDefault(x => x.Name == "All");
-			if (AccessRight is null)
-			{
-				context.Resources.Add(new Resource("All"));
+				context.Countries.Add(new Country("Sweden"));
 			}
 
 			var User = context.Users.FirstOrDefault(x => x.Name == "Bård B.");
 			if (User is null)
 			{
-				context.Users.Add(new User("Bård B.", "bård@dissimilis.no", 1, 1, new DateTime(1975, 08, 21)));
+				context.Users.Add(new User("Bård_diss", "Bård B.", "bård@dissimilis.no", 1, 1, new DateTime(1975, 08, 21)));
 			}
 
 			var Instrument = context.Instruments.FirstOrDefault(x => x.Name == "Piano");
 			if (Instrument is null)
 			{
 				context.Instruments.Add(new Instrument("Piano"));
+				context.Instruments.Add(new Instrument("Guitar"));
+				context.Instruments.Add(new Instrument("Bass"));
 			}
 
 			context.SaveChanges();
@@ -57,7 +69,23 @@ namespace Dissimilis.WebAPI.Database
 			var resourceGroup = context.UserGroupResources.FirstOrDefault(x => x.ResourceId == 1);
 			if (resourceGroup is null)
 			{
+				//Add the resources to the group they belong to 
+				//ADMIN:
 				context.UserGroupResources.Add(new UserGroupResources(1, 1));
+				context.UserGroupResources.Add(new UserGroupResources(2, 1));
+				context.UserGroupResources.Add(new UserGroupResources(3, 1));
+				context.UserGroupResources.Add(new UserGroupResources(4, 1));
+				context.UserGroupResources.Add(new UserGroupResources(5, 1));
+				
+				//GUEST:
+				context.UserGroupResources.Add(new UserGroupResources(1, 2));
+				
+				//INSTRUCTOR:
+				context.UserGroupResources.Add(new UserGroupResources(1, 3));
+				context.UserGroupResources.Add(new UserGroupResources(2, 3));
+				context.UserGroupResources.Add(new UserGroupResources(4, 3));
+				context.UserGroupResources.Add(new UserGroupResources(5, 3));
+
 			}
 
 			var MemberGroup = context.UserGroupMembers.FirstOrDefault(x => x.UserId == 1);
