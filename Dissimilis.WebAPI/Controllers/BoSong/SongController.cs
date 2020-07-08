368400d83e34f6770793225e141857d8748a2d66
Greger Martinussen <greger.martinussen@ciber.no>
2020-07-08 12:02:23 +0200

    Set file structure for controllers and mediators. Added functional prototype Song Controller with repository, DTO and mediator

---
+++ Dissimilis.WebAPI/Controllers/BoSong/SongController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dissimilis.WebAPI.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Dissimilis.WebAPI.Database.Models;
+using Dissimilis.WebAPI.Controllers.BoSong.Queries;
+
+namespace Dissimilis.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        //Private variable to get the DissimilisDbContext
+        private DissimilisDbContext context;
+        private IMediator _mediator;
+        public SongController(IMediator _mediator)
+        {
+            this._mediator = _mediator;
+        }
+
+        /// <summary>
+        /// Fetch all songs in the database
+        /// </summary>
+        /// <returns>200</returns>
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllSongs()
+        {
+            var SongDTOArray = await _mediator.Send(new QueryAllSongs());
+            return Ok(SongDTOArray);
+        }
+    }
+}
