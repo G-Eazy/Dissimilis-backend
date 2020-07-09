fe0fc5fd34cf347184b7010f8dcf93f461f6bdbc
Greger Martinussen <greger.martinussen@ciber.no>
2020-07-09 09:22:28 +0200

    Created SuperDTO object to be inherited from and used in controllers

---
+++ Dissimilis.WebAPI/Controllers/SuperDTOs/SuperDTO.cs
@@ -0,0 +1,21 @@
+﻿using Dissimilis.WebAPI.Database.Models;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dissimilis.WebAPI.Controllers.SuperDTOs
+{
+    public class SuperDTO
+    {
+        public int Id { get; set; }
+        public SuperDTO() { }
+        public SuperDTO(int Id) // TODO: generic?
+        {
+            this.Id = Id;
+        }
+
+
+    }
+}
