34170d43a63cb3b9c7e319c37520e87b8ab9f3bf
Greger Martinussen <greger.martinussen@ciber.no>
2020-07-23 09:43:47 +0200

    Added NewSong/UpdateSong validators

---
+++ Dissimilis.WebAPI/Repositories/Validators/NewPartDTOValidator.cs
@@ -0,0 +1,26 @@
+﻿using Dissimilis.WebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Dissimilis.WebAPI.Repositories.Validators
+{
+    public class NewPartDTOValidator : IValidator<NewPartDTO>
+    {
+        public NewPartDTOValidator() { }
+
+        public bool IsValid(NewPartDTO obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
+            if (obj.SongId <= 0) return false;
+            if (string.IsNullOrWhiteSpace(obj.Title)) return false;
+            if (obj.PartNumber <= 0) return false;
+            
+            return true;
+        }
+    }
+}
