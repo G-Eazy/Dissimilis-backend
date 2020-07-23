b3b3797190d70b149651f0f71ec90303c3104a61
Greger Martinussen <greger.martinussen@ciber.no>
2020-07-23 09:58:46 +0200

    Added NewBar/UpdateBar validators

---
+++ Dissimilis.WebAPI/Repositories/Validators/NewBarDTOValidator.cs
@@ -0,0 +1,26 @@
+﻿using Dissimilis.WebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Dissimilis.WebAPI.Repositories.Validators
+{
+    public class NewBarDTOValidator : IValidator<NewBarDTO>
+    {
+        public NewBarDTOValidator() { }
+
+        public bool IsValid(NewBarDTO obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
+            if (obj.BarNumber <= 0) return false;
+            if (obj.PartId <= 0) return false;
+            if (obj.House < 0) return false;
+            
+            return true;
+        }
+    }
+}
