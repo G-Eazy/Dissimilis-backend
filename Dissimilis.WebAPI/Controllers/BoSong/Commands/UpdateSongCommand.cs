d70ac9b8e8d76a39f36c6af3b7a6e5868aaf8bcc
Carl Valdemar Ebbesen <Carl.Valdemar.Ebbesen@ciber.no>
2021-06-29 17:31:58 +0200

    add logic update song

--- Dissimilis.WebAPI/Controllers/BoSong/Commands/UpdateSongCommand.cs
+++ Dissimilis.WebAPI/Controllers/BoSong/Commands/UpdateSongCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Dissimilis.WebAPI.Controllers.BoSong.DtoModelsIn;
 using Dissimilis.WebAPI.Extensions.Interfaces;
@@ -34,10 +35,31 @@ namespace Dissimilis.WebAPI.Controllers.BoSong
         public async Task<UpdatedSongCommandDto> Handle(UpdateSongCommand request, CancellationToken cancellationToken)
         {
             var song = await _repository.GetSongByIdForUpdate(request.SongId, cancellationToken);
-
+            if (!String.IsNullOrEmpty(request.Command.Title))
+            {
             song.Title = request.Command.Title;
-
-            song.SetUpdated(_IAuthService.GetVerifiedCurrentUser());
+            }
+            if (request.Command.SongNotes != null)
+            {
+                song.SongNotes = request.Command.SongNotes;
+            }
+            if (request.Command.Speed < 0)
+            {
+                throw new Exception("Song speed needs to be a positive number");
+            }
+            else if (request.Command.Speed != null)
+            {
+                song.Speed = request.Command.Speed;
+            }
+           if ( request.Command.DegreeOfDifficulty < 1 || request.Command.DegreeOfDifficulty > 10)
+            {
+                throw new Exception("Degree of difficulty needs to be a number between and inclusive 1 and 10");
+            }
+            else if ( request.Command.DegreeOfDifficulty != null)
+            {
+                song.DegreeOfDifficulty = request.Command.DegreeOfDifficulty;
+            }
+                song.SetUpdated(_IAuthService.GetVerifiedCurrentUser());
 
             await _repository.UpdateAsync(cancellationToken);
 
--- Dissimilis.WebAPI/Controllers/BoSong/DtoModelsIn/UpdateSongDto.cs
+++ Dissimilis.WebAPI/Controllers/BoSong/DtoModelsIn/UpdateSongDto.cs
@@ -10,19 +10,10 @@
         //public int Denominator { get; set; }
         //public string? SongNotes { get; set; }
 
-        // <summary>
-        // The speed of the song
-        // ( set a max range of 256) 
-        // </summary>
-        //[Range(0, 256)]
-        //public int? Speed { get; set; }
-        //
-        ///// <summary>
-        ///// how hard the song is to play
-        ///// (could change the range)
-        ///// </summary>
-        //[Range(1, 10)]
-        //public int? DegreeOfDifficulty { get; set; }
-        //
+        public int? Speed { get; set; }
+
+        public int? DegreeOfDifficulty { get; set; }
+        
+        public string SongNotes { get; set; }
     }
 }
