6fc487ea9dd80a327972aafe404f1df50aa87afb
Carl Valdemar Ebbesen <Carl.Valdemar.Ebbesen@ciber.no>
2021-06-29 15:17:47 +0200

    add comment for potential update song dto in

--- Dissimilis.WebAPI/Controllers/BoSong/DtoModelsIn/UpdateSongDto.cs
+++ Dissimilis.WebAPI/Controllers/BoSong/DtoModelsIn/UpdateSongDto.cs
@@ -8,6 +8,21 @@
         //public int Numerator { get; set; }
         //[Range(1, 10)]
         //public int Denominator { get; set; }
+        //public string? SongNotes { get; set; }
 
+        // <summary>
+        // The speed of the song
+        // ( set a max range of 256) 
+        // </summary>
+        //[Range(0, 256)]
+        //public int? Speed { get; set; }
+        //
+        ///// <summary>
+        ///// how hard the song is to play
+        ///// (could change the range)
+        ///// </summary>
+        //[Range(1, 10)]
+        //public int? DegreeOfDifficulty { get; set; }
+        //
     }
 }
