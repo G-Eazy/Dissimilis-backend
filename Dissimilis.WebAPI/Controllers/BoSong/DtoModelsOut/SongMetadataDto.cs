81c3faac4fcd10d60017ec5666af4f6e9dc923aa
Carl Valdemar Ebbesen <Carl.Valdemar.Ebbesen@ciber.no>
2021-07-06 13:15:25 +0200

    added metadata get endpoint

---
+++ Dissimilis.WebAPI/Controllers/BoSong/DtoModelsOut/SongMetadataDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Dissimilis.DbContext.Models.Song;
+
+namespace Dissimilis.WebAPI.Controllers.BoSong.DtoModelsOut
+{
+    public class SongMetadataDto
+    {
+        public int SongId { get; set; }
+        public string Title { get; set; }
+
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public string ArrangerName { get; set; }
+
+        public DateTimeOffset? UpdatedOn { get; set; }
+
+        public string SongNotes { get; set; }
+
+        public int? Speed { get; set; }
+
+        public int? DegreeOfDifficulty { get; set; }
+
+        public string Composer { get; set; }
+
+        public string Creator { get; set; }
+        public string UpdatedBy { get; set; }
+        public SongMetadataDto(Song song)
+        {
+            SongId = song.Id;
+            Title = song.Title;
+            ArrangerName = song.Arranger?.Name;
+            UpdatedOn = song.UpdatedOn;
+            UpdatedBy = song.UpdatedBy?.Name;
+            Numerator = song.Numerator;
+            Denominator = song.Denominator;
+            Speed = song.Speed;
+            DegreeOfDifficulty = song.DegreeOfDifficulty;
+            SongNotes = song.SongNotes;
+            Composer = song.Composer;
+            Creator = song.CreatedBy?.Name;
+        }
+    }
+}
