d07aac0346fd512d654771affa670ee1ae952070
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-07 10:10:50 +0200

    Created endpoint that will take undo-requests

--- Dissimilis.WebAPI/Controllers/BoVoice/VoiceController.cs
+++ Dissimilis.WebAPI/Controllers/BoVoice/VoiceController.cs
@@ -143,5 +143,17 @@ namespace Dissimilis.WebAPI.Controllers.BoVoice
             var result = await _mediator.Send(new QueryBarById(songId, voiceId, barId));
             return Ok(result);
         }
+
+        /// <summary>
+        /// Undo last action performed on a song
+        /// </summary>
+        [HttpDelete("song/{songId:int}/undo")]
+        [ProducesResponseType(typeof(SongVoiceDto), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Undo(int songId)
+        {
+            await _mediator.Send(new UndoCommand(songId));
+            var result = await _mediator.Send(new QuerySongUndoStackById(songId));
+            return Ok(result);
+        }
     }
 }
