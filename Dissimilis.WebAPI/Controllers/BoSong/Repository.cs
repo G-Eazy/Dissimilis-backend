da8f6281ebfa4387444bcb3ddccc368e52ddc5db
Carl Valdemar Ebbesen <Carl.Valdemar.Ebbesen@ciber.no>
2021-07-12 15:42:07 +0200

    add filtering function on accessing songs

--- Dissimilis.WebAPI/Controllers/BoSong/Repository.cs
+++ Dissimilis.WebAPI/Controllers/BoSong/Repository.cs
@@ -6,9 +6,8 @@ using Dissimilis.DbContext.Models.Song;
 using Dissimilis.WebAPI.Controllers.BoSong.DtoModelsIn;
 using Dissimilis.WebAPI.Exceptions;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System;
-using static Dissimilis.WebAPI.Extensions.Models.SongNoteExtension;
+using Dissimilis.DbContext.Models;
+using Dissimilis.DbContext.Models.Enums;
 
 namespace Dissimilis.WebAPI.Controllers.BoSong
 {
@@ -90,11 +89,31 @@ namespace Dissimilis.WebAPI.Controllers.BoSong
             await Context.SaveChangesAsync(cancellationToken);
         }
 
-        public async Task<Song[]> GetSongSearchList(SearchQueryDto searchCommand, CancellationToken cancellationToken)
+        private static bool AccessToSong(User user, Song song) {
+            return song.ProtectionLevel == ProtectionLevels.All
+            //include only songs within your organisations
+            || !song.SharedOrganisations.All(songOrg =>
+                user.Organisations.All(userOrg =>
+                songOrg.OrganisationId != userOrg.OrganisationId))
+            //include only songs within your groups
+            || !song.SharedGroups.All(songGroup =>
+                user.Groups.All(userGroup =>
+                songGroup.GroupId != userGroup.GroupId))
+            //include only songs shared with you by userSharing
+            || !song.SharedUsers.All(shared => shared.UserId != user.Id)
+            //include songs created by you
+            || song.ArrangerId == user.Id 
+            || song.CreatedById == user.Id;
+        }
+
+        public async Task<Song[]> GetSongSearchList(User user, SearchQueryDto searchCommand, CancellationToken cancellationToken)
         {
 
             var query = Context.Songs
                 .Include(s => s.Arranger)
+                .Include(s => s.SharedGroups)
+                .Include(s => s.SharedOrganisations)
+                .Where(song => AccessToSong(user, song))
                 .AsQueryable();
 
             if (!string.IsNullOrEmpty(searchCommand.Title))
