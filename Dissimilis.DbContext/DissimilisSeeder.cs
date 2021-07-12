90426b1d00030ea23d9d896c8a84e66181c3b8ad
Carl Valdemar Ebbesen <Carl.Valdemar.Ebbesen@ciber.no>
2021-07-12 12:30:50 +0200

    create new user if user doesnt exist, if existing => update

--- Dissimilis.DbContext/DissimilisSeeder.cs
+++ Dissimilis.DbContext/DissimilisSeeder.cs
@@ -10,7 +10,16 @@ namespace Dissimilis.DbContext
         {
             if (context.Users.All(x => !x.IsSystemAdmin))
             {
+                var adminUser = context.Users.SingleOrDefault(u => u.Email == "per.christian.kofstad@ciber.no");
+
+                if(adminUser == null)
+                {
                 context.Users.Add(new User() { Name = "AdminUser", Email = "per.christian.kofstad@ciber.no", IsSystemAdmin=true });
+                }
+                else
+                {
+                    adminUser.IsSystemAdmin = true;
+                }
             }
 
             context.SaveChanges();
