0259969ce70ff905a6c210badaf4ba7849af0c8d
Henrik Brun Fevang <Henrik.Brun.Fevang@ciber.no>
2021-07-16 11:22:51 +0200

    Changed to support adding a new chord when the first component interval is included, and delete on last component interval removed.

--- Dissimilis.WebAPI/Controllers/BoNote/DtoModelsIn/AddComponentIntervalNoteDto.cs
+++ Dissimilis.WebAPI/Controllers/BoNote/DtoModelsIn/AddComponentIntervalNoteDto.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Dissimilis.WebAPI.Controllers.BoVoice.DtoModelsIn
+namespace Dissimilis.WebAPI.Controllers.BoNote.DtoModelsIn
 {
     public class AddComponentIntervalNoteDto
     {
+        public string ChordName { get; set; }
+        public int NotePosition { get; set; }
+        public int Length { get; set; }
         public int IntervalPosition { get; set; }
     }
 }
