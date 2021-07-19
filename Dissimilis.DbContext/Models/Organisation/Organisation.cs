bdaa8846c33421d9803e566368866d2179ecc143
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-19 15:27:22 +0200

    Updated Organisation model to match requested fields

--- Dissimilis.DbContext/Models/Organisation/Organisation.cs
+++ Dissimilis.DbContext/Models/Organisation/Organisation.cs
@@ -24,6 +24,10 @@ namespace Dissimilis.DbContext.Models
         [MaxLength(100)]
         [Required]
         public string Name { get; set; }
+
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string Description { get; set; }
         
         /// <summary>
         /// The users in the organisation with their roles
