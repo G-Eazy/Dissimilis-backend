c98d72f6d4e1a3cdcbf00080937f2401ecae00d2
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-19 14:53:37 +0200

    Added endpoint to create group

---
+++ Dissimilis.WebAPI/Controllers/BoGroup/GroupController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Dissimilis.WebAPI.Controllers.BoGroup
+{
+    [Route("api/group")]
+    [ApiController]
+    public class GroupController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public GroupController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Create group
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(GroupByIdDto), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> CreateGroup([FromBody] CreateGroupDto command)
+        {
+            var item = await _mediator.Send(new CreateGroupCommand(command));
+            var result = await _mediator.Send(new QueryGroupById(item.SongId));
+
+            return Created($"{result.GroupId}", result);
+        }
+    }
+}
--- Dissimilis.WebAPI/Controllers/BoOrganisation/OrganisationController.cs
+++ Dissimilis.WebAPI/Controllers/BoOrganisation/OrganisationController.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Dissimilis.WebAPI.Controllers.BoOrganisation
 {
-    public class Class
+    [Route("api/organisation")]
+    [ApiController]
+    public class OrganisationController : Controller
     {
+        private readonly IMediator _mediator;
+
+        public OrganisationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(GroupByIdDto), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> CreateOrganisation([FromBody] CreateOrganisationDto command)
+        {
+            var item = await _mediator.Send(new CreateOrganisationCommand(command));
+            var result = await _mediator.Send(new QueryOrganisationById(item.OrganisationId));
+            return Created($"{result.OrganisationId}", result);
+        }
     }
 }
