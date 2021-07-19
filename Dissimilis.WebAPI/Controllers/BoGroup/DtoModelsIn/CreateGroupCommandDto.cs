d0fb5cdcffa0b752f9ce6c41a9dd55400872bd07
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-19 15:37:30 +0200

    Added createDtos for group and orgs

---
+++ Dissimilis.WebAPI/Controllers/BoGroup/DtoModelsIn/CreateGroupCommandDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Dissimilis.WebAPI.Controllers.BoGroup.DtoModelsIn
+{
+    public class CreateGroupCommandDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Dissimilis.WebAPI/Controllers/BoOrganisation/DtoModelsIn/CreateOrganisationCommandDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Dissimilis.WebAPI.Controllers.BoOrganisation.DtoModelsIn
+{
+    public class CreateGroupCommandDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public string Description { get; set; }
+    }
+}
