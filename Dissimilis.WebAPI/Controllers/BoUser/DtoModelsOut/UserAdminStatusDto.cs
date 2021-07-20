d13b8e12e5b4d34c2e12a95f05860bd1f2925536
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-20 14:10:38 +0200

    Added endpoint and logic to get admin statuses of current user

---
+++ Dissimilis.WebAPI/Controllers/BoUser/DtoModelsOut/UserAdminStatusDto.cs
@@ -0,0 +1,15 @@
+﻿using Dissimilis.DbContext.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dissimilis.WebAPI.Controllers.BoUser.DtoModelsOut
+{
+    public class UserAdminStatusDto
+    {
+        public bool SystemAdmin { get; set; }
+        public bool OrganisationAdmin { get; set; }
+        public bool GroupAdmin { get; set; }
+    }
+}
