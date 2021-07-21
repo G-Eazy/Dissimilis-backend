7aaef99c8c14cf99f9d587b99a504d4c38e1d05d
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-21 11:16:19 +0200

    yes

--- Dissimilis.WebAPI/Controllers/BoGroup/Query/QueryGroupById.cs
+++ Dissimilis.WebAPI/Controllers/BoGroup/Query/QueryGroupById.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Dissimilis.WebAPI.Controllers.BoGroup;
 using Dissimilis.WebAPI.Controllers.BoGroup.DtoModelsOut;
+using Dissimilis.WebAPI.Services;
 using MediatR;
 
 namespace Dissimilis.WebAPI.Controllers.Bogroup.Query
@@ -19,14 +20,17 @@ namespace Dissimilis.WebAPI.Controllers.Bogroup.Query
     public class QueryGroupByIdHandler : IRequestHandler<QueryGroupById, GroupByIdDto>
     {
         private readonly GroupRepository _groupRepository;
+        private readonly IAuthService _IAuthService;
 
-        public QueryGroupByIdHandler(GroupRepository repository)
+        public QueryGroupByIdHandler(GroupRepository repository, IAuthService authService)
         {
             _groupRepository = repository;
+            _IAuthService = authService;
         }
 
         public async Task<GroupByIdDto> Handle(QueryGroupById request, CancellationToken cancellationToken)
         {
+            var currentUser = _IAuthService.GetVerifiedCurrentUser();
             var result = await _groupRepository.GetGroupById(request.GroupId, cancellationToken);
 
             return new GroupByIdDto(result);
