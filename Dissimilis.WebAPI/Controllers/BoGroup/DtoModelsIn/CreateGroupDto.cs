2be2708755246137cf20db51e4b1db6538761a4d
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-22 09:53:29 +0200

    Added max lengths and required annotations for dto models in group/org

--- Dissimilis.WebAPI/Controllers/BoGroup/DtoModelsIn/CreateGroupDto.cs
+++ Dissimilis.WebAPI/Controllers/BoGroup/DtoModelsIn/CreateGroupDto.cs
@@ -1,9 +1,15 @@
-﻿namespace Dissimilis.WebAPI.Controllers.BoGroup.DtoModelsIn
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dissimilis.WebAPI.Controllers.BoGroup.DtoModelsIn
 {
     public class CreateGroupDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
         public int OrganisationId { get; set; }
+        [Required]
         public int FirstAdminId { get; set; }
     }
 }
--- Dissimilis.WebAPI/Controllers/BoOrganisation/DtoModelsIn/CreateOrganisationDto.cs
+++ Dissimilis.WebAPI/Controllers/BoOrganisation/DtoModelsIn/CreateOrganisationDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Dissimilis.WebAPI.Controllers.BoOrganisation.DtoModelsIn
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dissimilis.WebAPI.Controllers.BoOrganisation.DtoModelsIn
 {
     public class CreateOrganisationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]    
         public int FirstAdminId { get; set; }
     }
 }
