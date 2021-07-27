5840a8c643e6d5dcc3562e89d9d9b3daa41dfd52
Henrik Brun Fevang <Henrik.Brun.Fevang@ciber.no>
2021-07-27 13:58:15 +0200

    Fixed notePosition naming bug.

--- Dissimilis.WebAPI/Controllers/BoNote/Commands/ComponentInterval/RemoveComponentIntervalNoteCommand.cs
+++ Dissimilis.WebAPI/Controllers/BoNote/Commands/ComponentInterval/RemoveComponentIntervalNoteCommand.cs
@@ -59,8 +59,8 @@ namespace Dissimilis.WebAPI.Controllers.BoNote.Commands.ComponentInterval
             var songBar = songVoice.SongBars.FirstOrDefault(bar => bar.Position == request.SongBarPosition);
             if (songBar == null) throw new NotFoundException($"Bar with position {request.SongBarPosition} not found");
 
-            var songNote = songBar.Notes.FirstOrDefault(note => note.Position == request.Command.SongNotePosition);
-            if (songNote == null) throw new NotFoundException($"Note with position {request.Command.SongNotePosition} not found");
+            var songNote = songBar.Notes.FirstOrDefault(note => note.Position == request.Command.NotePosition);
+            if (songNote == null) throw new NotFoundException($"Note with position {request.Command.NotePosition} not found");
 
             if (songNote.ChordName == null)
             {
