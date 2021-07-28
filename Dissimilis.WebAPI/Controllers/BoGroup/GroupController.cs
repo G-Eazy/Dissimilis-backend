799b8bd5e9c10f4ae6f9c7724095fda9f6e97ee0
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-28 09:49:42 +0200

    Changed endpoint to delete group to not include orgid

--- Dissimilis.WebAPI/Controllers/BoGroup/GroupController.cs
+++ Dissimilis.WebAPI/Controllers/BoGroup/GroupController.cs
@@ -108,7 +108,7 @@ namespace Dissimilis.WebAPI.Controllers.BoGroup
             return Ok(group);
         }
 
-        [HttpDelete("{organisationId:int}/groups/{groupId:int}")]
+        [HttpDelete("groups/{groupId:int}")]
         [ProducesResponseType((int)HttpStatusCode.NoContent)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> DeleteGroup(int groupId)
