b9c11e8009e50130a767a734b7fcf5136593b0a6
Ole Jonas Liahagen <Ole.Jonas.Liahagen@ciber.no>
2021-07-30 12:46:52 +0200

    Updated endpoints and response types etc

--- Dissimilis.WebAPI/Controllers/BoNote/NoteController.cs
+++ Dissimilis.WebAPI/Controllers/BoNote/NoteController.cs
@@ -27,6 +27,7 @@ namespace Dissimilis.WebAPI.Controllers.BoNote
         /// Create note
         /// </summary>
         [HttpPost("bar/{barId:int}/note")]
+        [ProducesResponseType((int)HttpStatusCode.)]
         [ProducesResponseType(typeof(BarDto), (int)HttpStatusCode.Created)]
         public async Task<IActionResult> CreateNote(int songId, int voiceId, int barId, [FromBody] CreateNoteDto command)
         {
