40e2a5d2e5607049ad71cdd0d9e7938886fc36a8
Greger Martinussen <greger.martinussen@no.experis.com>
2022-05-14 13:27:19 +0200

    lolool

--- Dissimilis.WebAPI/Controllers/BoSong/Commands/CompileSongCommand.cs
+++ Dissimilis.WebAPI/Controllers/BoSong/Commands/CompileSongCommand.cs
@@ -19,7 +19,7 @@ namespace Dissimilis.WebAPI.Controllers.BoSong.Commands
     {
         public int songId { get; }
 
-        public CompileSongCommand (int songId)
+        public CompileSongCommand(int songId)
         {
             this.songId = songId;
         }
@@ -40,50 +40,130 @@ namespace Dissimilis.WebAPI.Controllers.BoSong.Commands
 
         public async Task<MemoryStream> Handle(CompileSongCommand request, CancellationToken cancellationToken)
         {
+
             var jsonObject = await _songRepository.GetSongById(request.songId, cancellationToken);
             var takt = jsonObject.Numerator;
-            var bpm = jsonObject.Speed;
+            var bpm1 = jsonObject.Speed;
+            var bpm = 120;
+            if (bpm1 is not null)
+                bpm = bpm1.Value;
 
 
             var notes = new List<string>();
+            var noteLengths = new List<TimeSpan>();
             foreach (var voice in jsonObject.Voices)
             {
-                foreach(var bar in voice.SongBars)
+                foreach (var bar in voice.SongBars)
                 {
-                    foreach(var note in bar.Notes)
+                    var positionCounter = 0;
+                    foreach (var note in bar.Notes)
                     {
-                        // Breaks on chords
+                        // Breaks on chords and long notes
+                        if (positionCounter != note.Position)
+                        {
+                            notes.Add("Z");
+                            noteLengths.Add(CalculateNoteLength(bpm, new TimeSpan(0, 0, 0, 1)));
+
+                        }
                         notes.Add(note.NoteValues);
+                        noteLengths.Add(CalculateNoteLength(bpm, new TimeSpan(0, 0, 0, note.Length)));
+
+                        positionCounter++;
                     }
                 }
 
-
                 break; //after first voice
             }
+
+
+            var noteArray = notes.ToArray();
+            var noteLengthArray = noteLengths.ToArray();
+
+            // Add sound file for each note
             var basePath = Path.Combine(Environment.CurrentDirectory, "SongFiles");
             var audioList = new List<AudioFileReader>();
-            foreach (var note in notes)
+            for (var i = 0; i < noteArray.Length; i++)
             {
-                var path = Path.Combine(basePath, "samples", note + ".mp3");
-                audioList.Add(new AudioFileReader(path));
+                var baseNote = Path.Combine(basePath, "samples", noteArray[i] + ".waw");
+                var outputNote = Path.Combine(basePath, "CutFiles", noteArray[i] + ".waw");
+                var noteDuration = noteLengthArray[i];
+                TrimWavFile(baseNote, outputNote, new TimeSpan(0, 0, 0, 0), noteDuration);
+
+
+                audioList.Add(new AudioFileReader(outputNote));
 
             }
 
-
+            // create songfile from notes
             var songName = jsonObject.Title;
             var playlist = new ConcatenatingSampleProvider(audioList);
             WaveFileWriter.CreateWaveFile16(Path.Combine(basePath, songName), playlist);
 
 
-
+            // Stream file
             var fileBytes = new byte[0];
-            string fileLocation = Path.Combine(Environment.CurrentDirectory,  "SongFiles", songName);
+            string fileLocation = Path.Combine(Environment.CurrentDirectory, "SongFiles", songName);
 
             var ms = new MemoryStream();
             var fs = new FileStream(fileLocation, FileMode.Open, FileAccess.Read);
             await fs.CopyToAsync(ms);
             return ms;
 
+        }
+
+        private static TimeSpan CalculateNoteLength(int bpm, TimeSpan noteLength)
+        {
+            var eights = ((1000 * 60) / bpm) / 2;
+            var len = noteLength.TotalSeconds;
+            var ms = len * eights;
+            if (ms < 1000)
+            {
+                return new TimeSpan(0, 0, 0, 0, (int) ms);
+            }
+            else
+            {
+                var baseval = (int) Math.Round(ms/1000);
+                return new TimeSpan(0, 0, 0, baseval, (int) (ms - baseval*1000) );
+            }
+        }
+        private static void TrimWavFile(string inPath, string outPath, TimeSpan cutFromStart, TimeSpan cutFromEnd)
+        {
+            var diff = (int) Math.Abs((new TimeSpan(0, 0, 0, 5) - cutFromEnd).TotalMilliseconds);
+            cutFromEnd = new TimeSpan(0, 0, 0, 0, diff);
+            using (WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using (WaveFileWriter writer = new WaveFileWriter(outPath, reader.WaveFormat))
+                {
+                    int bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond / 1000;
+
+                    int startPos = (int)cutFromStart.TotalMilliseconds * bytesPerMillisecond;
+                    startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+
+                    int endBytes = (int)cutFromEnd.TotalMilliseconds * bytesPerMillisecond;
+                    endBytes = endBytes - endBytes % reader.WaveFormat.BlockAlign;
+                    int endPos = (int)reader.Length - endBytes;
+
+                    TrimWavFile(reader, writer, startPos, endPos);
+                }
+            }
+        }
+        private static void TrimWavFile(WaveFileReader reader, WaveFileWriter writer, int startPos, int endPos)
+        {
+            reader.Position = startPos;
+            byte[] buffer = new byte[1024];
+            while (reader.Position < endPos)
+            {
+                int bytesRequired = (int)(endPos - reader.Position);
+                if (bytesRequired > 0)
+                {
+                    int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                    int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                    if (bytesRead > 0)
+                    {
+                        writer.WriteData(buffer, 0, bytesRead);
+                    }
+                }
+            }
         }
     }
 }
